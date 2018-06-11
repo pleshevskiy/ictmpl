642ba664050ed865ab9bf99b925b7661951d9b53
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2018-06-11 10:54:54 +0300

    Fix pip package

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
+import sys
 import os
 
 from setuptools import setup, find_packages
 
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    repo = '-r pypi'
+    if sys.argv[-2] == 'test':
+        repo = '-r pypitest'
+        
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload {} dist/*'.format(repo))
+    sys.exit()
 
 here = os.path.abspath(os.path.dirname(__file__))
 packages = find_packages()
@@ -19,10 +29,13 @@ setup(
     description=about['__description__'],
     long_description=long_description,
     long_description_content_type='text/markdown',
+    license=about['__license__'],
     author=about['__author__'],
     author_email=about['__author_email__'],
     url=about['__url__'],
     zip_safe=False,
+    package_data={'': ['LICENSE', 'README.md']},
+    include_package_data=True,
     packages=find_packages(),
     entry_points="""
     [console_scripts]
